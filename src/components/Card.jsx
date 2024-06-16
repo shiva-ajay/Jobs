8fcf7441b89abd42af59e41cc8db51d65334a7d3
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-16 23:54:13 +0530

    HomeCards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,7 @@
-import React, { Children } from 'react'
+import React, { Children } from "react";
 
-const Card = ({children}) => {
-  return (
-    <div className='bg-gray-100 p-6 rounded-lg shadow-md'>{children}</div>
-  )
-}
+const Card = ({ children, bg = "bg-gray-100" }) => {
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
+};
 
-export default Card
+export default Card;
