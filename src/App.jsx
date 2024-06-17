a459cb7634ccc9d4438b63f269836f9263df26ad
Shiva Ajay Veeragani <shivaajay987@gmail.com>
2024-06-17 22:39:11 +0530

    JobPage

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {
   import MainLayout from './layouts/MainLayout';
   import JobsPage from './pages/JobsPage';
   import NotFoundPage from './pages/NotFoundPage';
+  import JobPage from './pages/JobPage';
 
 
 const router = createBrowserRouter(
@@ -15,6 +16,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />} >
     <Route index element={<HomePage />} />
     <Route path='/jobs' element={<JobsPage />} />
+    <Route path='/jobs/:id' element={<JobPage />} />
     <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
